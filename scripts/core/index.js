ebd97bb19e9ad3ad211b3ea28ca5714669d5836c
zhi <linkasde_system@163.com>
2019-07-23 00:34:20 +0800

    :sparkles: elf's core

---
+++ scripts/core/index.js
@@ -0,0 +1,11 @@
+const elf = function(option, callback) {
+  const _option = option;
+
+  const entry = option.entry;
+  const output = option.output;
+
+  const modules = option.module || {};
+  const plugins = option.plugins || [];
+};
+
+module.exports = elf;
